e9468384a2ae709c205ca12c0858eba58d02c0f7
葛梦甜 <1711285659@qq.com>
2019-05-07 20:33:28 +0800

    简单框架

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>项目启动</h1>
-      <Button>点击按钮</Button>
-    </div>
+    <div>项目启动</div>
   );
 }
 
