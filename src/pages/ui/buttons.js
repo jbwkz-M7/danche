4fd7504c5e0feb8900bce5de5498fcb2663a3e97
葛梦甜 <1711285659@qq.com>
2019-05-08 21:00:15 +0800

    button

--- src/pages/ui/buttons.js
+++ src/pages/ui/buttons.js
@@ -1,30 +1,15 @@
 import React from 'react'
 import {Card,Button,Radio} from 'antd'
-// import './ui.less'
+import './ui.less'
 export default class Buttons extends React.Component {
-
-    // state = {
-    //     loading:true,
-    //     size:'default'
-    // }
-
-    // handleCloseLoading=()=>{
-    //     this.setState({
-    //         loading:false
-    //     });
-    // }
-
-    // handleChange = (e)=>{
-    //     this.setState({
-    //         size:e.target.value
-    //     })
-    // }
+    state = {
+        loading:true
+    }
 
     render() {
         return (
             <div>
-                this is button page.
-                {/* <Card title="基础按钮" className="card-wrap">
+                <Card title="基础按钮" className="card-wrap">
                     <Button type="primary">Imooc</Button>
                     <Button>Imooc</Button>
                     <Button type="dashed">Imooc</Button>
@@ -39,12 +24,12 @@ export default class Buttons extends React.Component {
                     <Button type="primary" icon="search">搜索</Button>
                     <Button type="primary" icon="download">下载</Button>
                 </Card>
-                <Card title="Loading按钮" className="card-wrap">
-                    <Button type="primary" loading={this.state.loading}>确定</Button>
-                    <Button type="primary" shape="circle" loading={this.state.loading}></Button>
+                <Card title="Loading按钮" className="card-wrap" >
+                    <Button type="primary" loading={this.state.loading} >确定</Button>
+                    <Button type="primary" shape="circle" loading={this.state.loading}> </Button>
                     <Button loading={this.state.loading} >点击加载</Button>
-                    <Button shape="circle" loading={this.state.loading}></Button>
-                    <Button type="primary" onClick={this.handleCloseLoading}>关闭</Button>
+                    <Button shape="circle" loading={this.state.loading} ></Button>
+                    <Button type="primary" onClick={this.handleCloseLoading.bind(this)} >关闭</Button>
                 </Card>
                 <Card title="按钮组" style={{marginBottom:10}}>
                     <Button.Group>
@@ -53,17 +38,23 @@ export default class Buttons extends React.Component {
                     </Button.Group>
                 </Card>
                 <Card title="按钮尺寸" className="card-wrap">
-                    <Radio.Group value={this.state.size} onChange={this.handleChange}>
+                    <Radio.Group >
                         <Radio value="small">小</Radio>
                         <Radio value="default">中</Radio>
                         <Radio value="large">大</Radio>
                     </Radio.Group>
-                    <Button type="primary" size={this.state.size}>Imooc</Button>
-                    <Button size={this.state.size}>Imooc</Button>
-                    <Button type="dashed" size={this.state.size}>Imooc</Button>
-                    <Button type="danger" size={this.state.size}>Imooc</Button>
-                </Card> */}
+                    <Button type="primary" >Imooc</Button>
+                    <Button >Imooc</Button>
+                    <Button type="dashed" >Imooc</Button>
+                    <Button type="danger" >Imooc</Button>
+                </Card>
             </div>
         );
+    }
+
+    handleCloseLoading() {
+        this.setState({
+            loading:false
+        })
     }
 }
--- src/router.js
+++ src/router.js
@@ -5,20 +5,27 @@ import Login from './pages/login'
 import Admin from './admin'
 import Buttons from './pages/ui/buttons'
 import NoMatch from './pages/nomatch'
+import Home from './pages/home'
 
 export default class IRouter extends React.Component {
   render() {
     return (
       <Router>
         <App>
-          <Route path="/login" component={Login} />
-          <Route path="/admin" render={() =>
-            <Admin>
-              <Route path="/admin/ui/buttons" component={Buttons} />
-              <Route component={NoMatch} />
-            </Admin>
-          } />
-          <Route path="/order/detail" component={Login} />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/" render={()=>
+                <Admin>
+                  <Switch>
+                    <Route path='/home' component={Home} />
+                    <Route path="/ui/buttons" component={Buttons} />
+                    <Redirect to="/home" />
+                    {/* <Route component={NoMatch} /> */}
+                  </Switch>
+                </Admin>
+            } />
+            <Route path="/order/detail" component={Login} />
+          </Switch>
         </App>
       </Router>
     )
